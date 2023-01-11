065886d9945ce5e2289c8bce88aae450aa946229
Tạ Điền Minh Trí <91335704+tritdm@users.noreply.github.com>
2023-01-11 11:24:35 +0700

    Update main.c

--- gpio/REG/src/main.c
+++ gpio/REG/src/main.c
@@ -10,63 +10,64 @@ void delay(uint32_t u32msDelay);
 
 int main(void) 
 {		
-		_GPIO_Init();
-		Timer_Init();
-		while (1) 
+	_GPIO_Init();
+	Timer_Init();
+	while (1) 
+	{
+		/* Software Debounce */
+		if (1u == (GPIOA->IDR & 1u)) 
 		{
-				/* Software Debounce */
-				if (1u == (GPIOA->IDR & 1u)) 
+			delay(50u);
+			if (1u == (GPIOA->IDR & 1u)) 
+			{
+				while (1u == (GPIOA->IDR & 1u)) 
 				{
-						delay(50u);
-						if (1u == (GPIOA->IDR & 1u)) 
-						{
-								while (1u == (GPIOA->IDR & 1u)) 
-								{
-										/* waiting */
-								}
-								GPIOD->ODR ^= (1u << 12);
-						}
+					/* waiting */
 				}
+				GPIOD->ODR ^= (1u << 12);
+			}
 		}
+	}
 }
 
 void _GPIO_Init(void) 
 {
-		/* GPIOD for built-in led and GPIOA for built-in user button */
-		/* GPIOD Clock */
-		RCC->AHB1ENR 		|= 	(1u << 3);
-		/* GPIOD config */
-		GPIOD->MODER 		&= 	~(3u << 24);
-		GPIOD->MODER 		|= 	(1u << 24);
-		GPIOD->OTYPER 		&= 	~(1u << 12);
-		/* GPIOA Clock */
-		RCC->AHB1ENR 		|= 	(1u << 0);
-		/* GPIOA config */
-		GPIOA->MODER 		&= 	~(3u << 0);
-		GPIOA->PUPDR 		&= 	~(3u << 0);
+	/* GPIOD for built-in led and GPIOA for built-in user button */
+	/* GPIOD Clock */
+	RCC->AHB1ENR 		|= 	(1u << 3);
+	/* GPIOD config */
+	GPIOD->MODER 		&= 	~(3u << 24);
+	GPIOD->MODER 		|= 	(1u << 24);
+	GPIOD->OTYPER 		&= 	~(1u << 12);
+	/* GPIOA Clock */
+	RCC->AHB1ENR 		|= 	(1u << 0);
+	/* GPIOA config */
+	GPIOA->MODER 		&= 	~(3u << 0);
+	GPIOA->PUPDR 		&= 	~(3u << 0);
 }
 
 void Timer_Init(void) 
 {
-		/* Timer 6 for delay */
-		/* Timer 6 Clock */
-		RCC->APB1ENR 		|= (1u << 4);
-		/* Timer 6 config */
-		TIM6->CR1 		|= 	(1u << 1);
-		TIM6->PSC 		= 	(84u - 1u);
-		TIM6->CR1 		&= 	~(1u << 1);
-		TIM6->EGR 		= 	(1u << 0);
-		TIM6->CR1 		|= 	(1u << 0);
+	/* Timer 6 for delay */
+	/* Timer 6 Clock */
+	RCC->APB1ENR 		|= (1u << 4);
+	/* Timer 6 config */
+	TIM6->CR1 		|= 	(1u << 1);
+	TIM6->PSC 		= 	(84u - 1u);
+	TIM6->CR1 		&= 	~(1u << 1);
+	TIM6->EGR 		= 	(1u << 0);
+	TIM6->CR1 		|= 	(1u << 0);
 }
 
 void delay(uint32_t u32msDelay) 
 {
-		while (u32msDelay) {
-				TIM6->CNT = 0u;
-				while (TIM6->CNT < 1000u) 
-				{
-						/* waiting */
-				}
-				-- u32msDelay;
+	while (u32msDelay) 
+	{
+		TIM6->CNT = 0u;
+		while (TIM6->CNT < 1000u) 
+		{
+			/* waiting */
 		}
+		-- u32msDelay;
+	}
 }
